08bb5dcc93fe32a85e751e5e1e890a3ffdc4ea50
H2O <44346172+tonyH2O@users.noreply.github.com>
2022-07-11 23:01:50 +0300

    Add files via upload

---
+++ day2/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+  unsigned int counter;
+  int grade;
+  int total;
+  int average;
+
+  total = 0;
+  counter = 1;
+
+  while(counter<=10)
+  {
+    printf("%s", "Enter grade: ");
+    scanf("%d", &grade);
+    total+=grade;
+    counter+=1;
+
+   }
+average = total/10;
+printf("Class average is %d\n", average);
+
+
+
+   }
