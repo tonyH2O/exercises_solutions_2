f004e73368185471fe37a5ce6e2c45768ceb9222
H2O <44346172+tonyH2O@users.noreply.github.com>
2022-07-12 22:52:02 +0300

    Add files via upload

---
+++ day3/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a,b;
+  printf("Enter 1:\n ");
+  scanf("%d",&a);
+
+  printf("Read 2:\n ");
+  scanf("%d",&b);
+  
+  int sum=a+b;
+  printf("Sum is: %d\n",sum);
+}
+
+
